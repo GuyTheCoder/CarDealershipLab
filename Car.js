eec3e2b113617e3ea9458e8cf753e82d840bd73e
Guy Chalk <guychalk532@gmail.com>
2022-09-23 08:25:19 +0100

    uploading the lab

---
+++ Car.js
@@ -0,0 +1,9 @@
+const Car = function(manufacturer, price, engine){
+    
+    this.manufacturer = manufacturer;
+    this.price = price;
+    this.engine = engine;
+
+    //
+
+}
---
+++ Dealership.js
@@ -0,0 +1,23 @@
+const Dealership = function(name, capacity, numberCurrentlyInStock){
+    
+    this.name = name;
+    this.capacity = capacity;
+    this.numberCurrentlyInStock = numberCurrentlyInStock;
+
+    // count number of cars in stock
+
+    function(countCars){
+
+    } 
+
+    // add a car to stock
+
+    function(addCar){
+        
+    }
+
+    // return array containing each cars manufacturer
+
+    // total value of all cars in stock
+
+}
